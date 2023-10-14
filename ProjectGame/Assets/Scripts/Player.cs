81c8f8304af327366a754e1841862b9c0dea1ad7
Codekxe <legiakhang23072011.thhungdao@gmail.com>
2023-10-14 11:51:42 +0700

    +Animation +Level3

--- ProjectGame/Assets/Scripts/Player.cs
+++ ProjectGame/Assets/Scripts/Player.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public float speed = 5.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,9 +15,21 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetKey(KeyCode.LeftArrow) | Input.GetKey(KeyCode.A))
         {
-            transform.Translate(5, 0, 0);
+            transform.Translate(-speed * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.RightArrow) | Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(speed * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.UpArrow) | Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(0, speed * Time.deltaTime, 0);
+        }
+        if (Input.GetKey(KeyCode.DownArrow) | Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0, -speed * Time.deltaTime, 0);
         }
     }
 }
